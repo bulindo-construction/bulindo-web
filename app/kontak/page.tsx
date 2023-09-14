30c4dfc881a5b2b57712bc9f446687c64cd5e15b
BaconLover307 <kresnadi30@gmail.com>
2023-09-14 15:40:59 +0700

    Implemented Contact page pt1

---
+++ app/kontak/page.tsx
@@ -0,0 +1,68 @@
+import { StaticJumbotron } from "@/app/components/carousel";
+import contactHighlight from "@/public/jumbotron/Jumbo-1.png";
+import icon_email from "@/public/icons/alternate_email.svg";
+import icon_cs from "@/public/icons/support_agent.svg";
+import icon_alamat from "@/public/icons/apartment.svg";
+import styles from "./styles/Contact.module.css";
+import Image from "next/image";
+
+const ICON_HEIGHT = 200;
+
+export default function Contact() {
+  return (
+    <main className="flex flex-col items-center z-0">
+      <StaticJumbotron
+        imgSource={contactHighlight}
+        className={"h-[30vh]"}
+        isDark
+      >
+        <h1 className="text-white-1">Hubungi Kami</h1>
+      </StaticJumbotron>
+      <div className="w-full py-16 bg-secondary-1-light flex justify-center">
+        <div className="lockup px-10 xl:px-0 inline-grid grid-cols-3 gap-8">
+          <div className={styles.contact_container}>
+            <Image
+              src={icon_alamat}
+              alt=""
+              height={ICON_HEIGHT}
+              className={styles.contact_icon}
+            />
+            <h3 className={styles.contact_title}>Alamat Kantor</h3>
+            <p className={styles.contact_content}>
+              Jalan NamaJalan No. 1234
+              <br />
+              Kel. Lurah, Kec. Camat
+              <br />
+              Bandung - 14440
+            </p>
+          </div>
+          <div className={styles.contact_container}>
+            <Image
+              src={icon_cs}
+              alt=""
+              height={ICON_HEIGHT}
+              className={styles.contact_icon}
+            />
+            <h3 className={styles.contact_title}>Customer Service</h3>
+            <p className={styles.contact_content}>
+              No telp. - (+62) 888 0800 8080
+              <br />
+              Fax. - (+62) 888 0800 8080
+            </p>
+          </div>
+          <div className={styles.contact_container}>
+            <Image
+              src={icon_email}
+              alt=""
+              height={ICON_HEIGHT}
+              className={styles.contact_icon}
+            />
+            <h3 className={styles.contact_title}>Email</h3>
+            <p className={styles.contact_content}>construction@gmail.com</p>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </main>
+  );
+}
