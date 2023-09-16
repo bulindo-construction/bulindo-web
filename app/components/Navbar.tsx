39176b0ea532f59b313f86f8f4a6f15892c3d82c
BaconLover307 <kresnadi30@gmail.com>
2023-09-16 10:15:59 +0700

    Refined navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ import classNames from "classnames";
 import logo from "@/public/logo-nav.png";
 
 const isBrowser = () => typeof window !== "undefined";
-const SCROLL_HEIGHT_TRANSITION = 120;
+const SCROLL_HEIGHT_TRANSITION = 200;
 const LOGO_WIDTH = 136;
 const LOGO_HEIGHT = 40;
 
@@ -29,11 +29,13 @@ const Navbar = () => {
 
   var navClassname = classNames(
     {
-      "bg-white-1/[.85]": navTransparent,
+      "bg-white-1/[.75]": navTransparent,
       "bg-white-1": !navTransparent,
     },
     "flex text-dark-1 justify-end items-center gap-6 py-2 px-6 w-full drop-shadow-md fixed top-0 z-50 overflow-hidden transition duration-300"
   );
+
+  var navItemClassname = "hover:transition-all duration-150 hover:underline";
 
   return (
     <nav id="nav" className={navClassname}>
@@ -46,11 +48,22 @@ const Navbar = () => {
           src={logo}
         />
       </Link>
-      <Link href="/">Beranda</Link>
-      <Link href="/profil">Profil</Link>
-      <Link href="/layanan">Layanan</Link>
-      <Link href="/portofolio">Portofolio</Link>
-      <Link className="px-5 py-3 bg-primary-1 text-white-1" href="/kontak">
+      <Link className={navItemClassname} href="/">
+        Beranda
+      </Link>
+      <Link className={navItemClassname} href="/profil">
+        Profil
+      </Link>
+      <Link className={navItemClassname} href="/layanan">
+        Layanan
+      </Link>
+      <Link className={navItemClassname} href="/portofolio">
+        Portofolio
+      </Link>
+      <Link
+        className="px-4 py-2 bg-primary-1 text-white-1 hover:transition-all duration-150 hover:scale-105"
+        href="/kontak"
+      >
         Hubungi Kami
       </Link>
     </nav>
