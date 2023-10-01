0e315d6db5bc664d1ba12eec1a09ce82bab24bca
BaconLover307 <kresnadi30@gmail.com>
2023-10-01 14:53:49 +0700

    Fixed client side errors on build

--- app/kontak/Map.tsx
+++ app/kontak/Map.tsx
@@ -18,16 +18,21 @@ const COORDINATES = { lat: -6.892294202219886, lng: 107.61265631349268 };
 const MAP_URL =
   "https://www.google.com/maps/dir//Two+Hands+Full,+Jl.+Ir.+H.+Juanda+No.113,+Lebak+Siliwangi,+Coblong,+Bandung+City,+West+Java+40132/@-6.8924806,107.6126885,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x2e68e68ca3e607d3:0x1ad911ec6c6ccf0b!2m2!1d107.6126892!2d-6.8924807!3e0?entry=ttu";
 
-const containerStyle = {
-  width: "100vw",
-  height: window.innerWidth < 768 ? "70vh" : "50vh",
-};
-
 function Map() {
   const center = useMemo(() => COORDINATES, []);
   const [selectedMarker, setSelectedMarker] = useState<Coordinates | null>(
     null
   );
+
+  const containerStyle = useMemo(() => {
+    return {
+      width: "100vw",
+      height:
+        typeof window !== "undefined" && window.innerWidth < 768
+          ? "70vh"
+          : "50vh",
+    };
+  }, []);
 
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
@@ -36,12 +41,17 @@ function Map() {
 
   const [map, setMap] = useState<google.maps.Map | null>(null);
 
-  const onLoad = useCallback((map: google.maps.Map) => {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+  const onLoad = useCallback(
+    (map: google.maps.Map) => {
+      if (typeof window !== "undefined") {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
 
-    setMap(map);
-  }, []);
+        setMap(map);
+      }
+    },
+    [center]
+  );
 
   const onUnmount = useCallback((map: google.maps.Map) => {
     setMap(null);
